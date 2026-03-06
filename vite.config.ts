ea76a6d6fa018edfd84c06db3a69aeed1469d2a9
YourName <you@example.com>
2026-03-06 15:49:25 +0700

    Secure API key with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
       base: "/inpaintPMS/",
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
