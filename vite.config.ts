3527aa488b201c7cb4633b63fa6259fbbdd41a37
YourName <you@example.com>
2026-03-06 16:23:25 +0700

    Update-config-and-deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/inpaintPMS/',
       server: {
-        port: 3001,
+        port: 5173,
         host: '0.0.0.0',
       },
-      base: "/inpaintPMS/",
       plugins: [react()],
       define: {
-        'process.env.API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY),
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
